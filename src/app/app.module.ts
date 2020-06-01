fb5abbe536cd2e57d913af15ae6da1e3402fe4ef
mfcodeworks <arranjamesfletcher2012@gmail.com>
2020-06-01 20:18:49 +0800

    Move providers to shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,20 @@
-import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireMessagingModule } from '@angular/fire/messaging';
 import { RouteReuseStrategy } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material/material.module';
-import { JwtInterceptor } from './shared/interceptors/jwt.interceptor';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ZoomService } from './services/zoom/zoom.service';
-import { ThemeService } from './services/theme/theme.service';
-import { CacheService } from './services/cache/cache.service';
-import { NotificationService } from './services/notification/notification.service';
-import { environment } from '../environments/environment';
-import { NetworkService } from './services/network/network.service';
 import { SharedModule } from './shared/shared.module';
-import { AuthService } from './services/auth/auth.service';
-import { BalanceService } from './services/balance/balance.service';
-import { TransactionsService } from './services/transactions/transactions.service';
-import { SourcesService } from './services/sources/sources.service';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { JwtInterceptor } from './shared/interceptors/jwt.interceptor';
+import { environment } from '../environments/environment';
 
 @NgModule({
     imports: [
@@ -44,46 +33,8 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
         AppComponent
     ],
     providers: [
-        SplashScreen,
-        BarcodeScanner,
         { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-        {
-            provide: APP_INITIALIZER,
-            useFactory: (
-                z: ZoomService,
-                t: ThemeService,
-                c: CacheService,
-                n: NotificationService,
-                net: NetworkService,
-                a: AuthService,
-                b: BalanceService,
-                tr: TransactionsService,
-                s: SourcesService
-            ) => () => Promise.all([
-                net.init(),
-                c.init(),
-                z.init(),
-                t.init(),
-                n.init(),
-                a.init(),
-                b.init(),
-                tr.init(),
-                s.init(),
-            ]),
-            deps: [
-                ZoomService,
-                ThemeService,
-                CacheService,
-                NotificationService,
-                NetworkService,
-                AuthService,
-                BalanceService,
-                TransactionsService,
-                SourcesService
-            ],
-            multi: true
-        }
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
     ],
     bootstrap: [AppComponent]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { LongholdDirective } from './directives/longhold.directive';
 import { RouteTransformerDirective } from './directives/route-transformer.directive';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { ZoomService } from '../services/zoom/zoom.service';
+import { ThemeService } from '../services/theme/theme.service';
+import { CacheService } from '../services/cache/cache.service';
+import { NotificationService } from '../services/notification/notification.service';
+import { NetworkService } from '../services/network/network.service';
+import { AuthService } from '../services/auth/auth.service';
+import { BalanceService } from '../services/balance/balance.service';
+import { TransactionsService } from '../services/transactions/transactions.service';
+import { SourcesService } from '../services/sources/sources.service';
 
 @NgModule({
     imports: [
@@ -14,6 +25,46 @@ import { RouteTransformerDirective } from './directives/route-transformer.direct
         LongholdDirective,
         RouteTransformerDirective,
         TopBarComponent
+    ],
+    providers: [
+        SplashScreen,
+        BarcodeScanner,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: (
+                z: ZoomService,
+                t: ThemeService,
+                c: CacheService,
+                n: NotificationService,
+                net: NetworkService,
+                a: AuthService,
+                b: BalanceService,
+                tr: TransactionsService,
+                s: SourcesService
+            ) => () => Promise.all([
+                net.init(),
+                c.init(),
+                z.init(),
+                t.init(),
+                n.init(),
+                a.init(),
+                b.init(),
+                tr.init(),
+                s.init(),
+            ]),
+            deps: [
+                ZoomService,
+                ThemeService,
+                CacheService,
+                NotificationService,
+                NetworkService,
+                AuthService,
+                BalanceService,
+                TransactionsService,
+                SourcesService
+            ],
+            multi: true
+        }
     ],
     exports: [
         LongholdDirective,
